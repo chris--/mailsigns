46416a23479fb7dc813c151ff0c7e88b2bc4af2c
Christian Vogt <mail@christianvogt.de>
2018-01-14 20:20:46 +0100

    Fixed copy button

--- src/app/user/SignatureUserOutput.js
+++ src/app/user/SignatureUserOutput.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Mustache from 'mustache';
-import CopyButton from './CopyButton';
 import PropTypes from 'prop-types';
+import Clipboard from 'clipboard-polyfill';
+
+const MSG_TEXT_PASTE = 'You need to paste this signature directly into your mail client';
+const COPY_BUTTON_TEXT = 'Copy to Clipboard';
+const COPY_BUTTON_TEXT_SUCCESS = 'Copied!';
+const COPY_BUTTON_TEXT_ERROR = 'Copy failed!';
 
 export default class SignatureUserOutput extends React.Component {
+
   constructor(props) {
     if (props.signature && props.signature.template && props.signature.initials) {
       // do nothing
@@ -11,13 +17,40 @@ export default class SignatureUserOutput extends React.Component {
     super(props);
     this.state = {
       rawHTML: '<p>No signature selected</p>',
+      buttonAdditionalClassName: 'btn-primary',
+      buttonText: COPY_BUTTON_TEXT
     };
+    this.resetState.bind(this);
   }
+
   componentWillReceiveProps(nextProps) {
     this.setState({
       rawHTML: Mustache.render(nextProps.signature.template, nextProps.signature.initials),
     });
   }
+
+  resetState = (setAfterTimeout) => {
+    if (setAfterTimeout) {
+      setTimeout(this.resetState, setAfterTimeout);
+    } else {
+      this.setState({ buttonText: COPY_BUTTON_TEXT, buttonAdditionalClassName: 'btn-primary' });
+    }
+  }
+
+  onCopy = (e) => {
+    var dt = new Clipboard.DT();
+    dt.setData("text/plain", MSG_TEXT_PASTE);
+    dt.setData("text/html", this.state.rawHTML);
+    Clipboard.write(dt)
+    .then(() => {
+      this.setState({ buttonText: COPY_BUTTON_TEXT_SUCCESS, buttonAdditionalClassName: 'btn-success' });
+      this.resetState(2000);
+    }).catch(() => {
+      this.setState({ buttonText: COPY_BUTTON_TEXT_ERROR, buttonAdditionalClassName: 'btn-error' });
+      this.resetState(2000);
+    });
+  };
+
   render() {
     return (<div className="panel panel-primary">
       <div className="panel-heading">Preview</div>
@@ -31,16 +64,16 @@ export default class SignatureUserOutput extends React.Component {
         </form>
         <br />
         <form>
-          <CopyButton
+          <button
             type="button"
-            className="btn btn-primary"
-            data-clipboard-target={this.props.outputTemplateId}
-          >Copy to Clipboard</CopyButton>
+            className={`btn ${this.state.buttonAdditionalClassName}`}
+            onClick={this.onCopy}>{this.state.buttonText}</button>
         </form>
       </div>
     </div>);
   }
 }
+
 SignatureUserOutput.propTypes = {
   signature: PropTypes.shape({
     template: PropTypes.string,
@@ -48,6 +81,7 @@ SignatureUserOutput.propTypes = {
   }).isRequired,
   outputTemplateId: PropTypes.string,
 };
+
 SignatureUserOutput.defaultProps = {
   outputTemplateId: `signature-output-${Math.round(Math.random() * 10000)}`,
 };
