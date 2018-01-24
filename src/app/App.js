6fe6035ffb5129a6a46341b7d438808d2d31d683
Christian Vogt <mail@christianvogt.de>
2018-01-24 10:50:43 +0100

    Remove admin path

--- src/app/App.js
+++ src/app/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 import UserPage from "./user/UserPage";
-import AdminPage from './admin/AdminPage';
 import Navbar from './navigation/Navbar';
 
 const App = (props) => (
   <React.Fragment>
     <Route component={Navbar} />
     <Route exact path='/' component={UserPage}/>
-    <Route exact path='/admin' component={AdminPage}/>
   </React.Fragment>
 );
 
